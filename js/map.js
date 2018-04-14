f290c8ba5224ac147b1cbe7371d13237b6a49f17
Pavel Rogovin <rogovin.pavel@gmail.com>
2018-04-14 15:04:24 +0300

    drag and drop

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 'use strict';
-var PIN_X = 10;
-var PIN_Y = 30;
+var PIN_X = 20;
+var PIN_Y = 44;
 var X_MIN = 300;
 var X_MAX = 900;
 var Y_MIN = 150;
@@ -248,6 +248,41 @@ mapActivate.addEventListener('mouseup', function () {
   renderPins(flats);
   mapPinsList.appendChild(pinsFragment);
   mapCardList.appendChild(renderNewCard(flats[0]));
+});
+
+mapActivate.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mapActivate.style.top = (mapActivate.offsetTop - shift.y) + 'px';
+    mapActivate.style.left = (mapActivate.offsetLeft - shift.x) + 'px';
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
 
 mapPinsList.addEventListener('click', function (evt) {
