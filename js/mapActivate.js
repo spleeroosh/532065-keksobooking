49e88e0bffc7aaa72b32bc77cf9ba7f198dfe2ef
Pavel Rogovin <rogovin.pavel@gmail.com>
2018-05-01 17:57:59 +0300

    Рефакторинг (Travis)

--- js/mapActivate.js
+++ js/mapActivate.js
@@ -7,7 +7,6 @@
   var mapActivate = document.querySelector('.map__pin--main');
   window.form.querySelector('#address').value = parseInt(mapActivate.style.left + window.PIN_X, 10) + ', ' + parseInt(mapActivate.style.top + window.PIN_Y, 10);
   var fieldsetArray = window.form.querySelectorAll('fieldset');
-  console.log(fieldsetArray);
   window.flatsData = [];
 
   mapActivate.addEventListener('mouseup', function () {
--- js/reset.js
+++ js/reset.js
@@ -13,14 +13,14 @@
     }
     window.card.remove(); // Скрываем карточку, при изменении фильтров
     window.pins.remove(); // Удаляем метки с карты
-    form.querySelector('#type')[1].selected = true;
-    form.querySelector('#price').value = '';
-    form.querySelector('#price').min = 0;
-    form.querySelector('#price').placeholder = 0;
-    form.querySelector('#title').value = '';
-    form.querySelector('#timein').value = '12:00';
-    form.querySelector('#timeout').value =  form.querySelector('#timein').value;
-    form.querySelector('#description').value = '';
+    window.form.querySelector('#type')[1].selected = true;
+    window.form.querySelector('#price').value = '';
+    window.form.querySelector('#price').min = 0;
+    window.form.querySelector('#price').placeholder = 0;
+    window.form.querySelector('#title').value = '';
+    window.form.querySelector('#timein').value = '12:00';
+    window.form.querySelector('#timeout').value = window.form.querySelector('#timein').value;
+    window.form.querySelector('#description').value = '';
 
     var roomNumber = form.querySelector('#room_number');
     var roomCapacity = form.querySelector('#capacity');
@@ -33,7 +33,7 @@
 
     document.querySelector('.map__pin--main').style.left = '570px';
     document.querySelector('.map__pin--main').style.top = '375px';
-    form.querySelector('#address').value = '570, 375';
+    window.form.querySelector('#address').value = '570, 375';
 
     var featuresChecked = form.querySelectorAll('.feature__checkbox');
     for (var i = 0; i < featuresChecked.length; i++) {
